d3498f64b4884a742e278ac9920befc00ee2976c
shngmsw <mose1021@gmail.com>
2022-09-26 12:36:20 +0900

    fix:cache -> fetch

--- index.js
+++ index.js
@@ -11,42 +11,34 @@ const client = new Client({
   ],
 });
 // Initialize the invite cache
-const guildInvites = new Map();
+const invites = {};
 
 // A pretty useful method to create a delay without blocking the whole script.
 const wait = require("util").promisify(setTimeout);
-
 client.on("ready", async () => {
   // "ready" isn't really ready. We need to wait a spell.
   await wait(1000);
   console.log(`Logged in as ${client.user.tag}!`);
 
-  client.guilds.cache.forEach(guild => {
-    guild.invites.fetch()
-      .then(invites => {
-        console.log("INVITES CACHED");
-        const codeUses = new Map();
-        invites.each(inv => codeUses.set(inv.code, inv.uses));
-        guildInvites.set(guild.id, codeUses);
-      })
-      .catch(err => {
-        console.log("OnReady Error:", err)
-      })
-  })
+  client.guilds.cache.forEach(g => {
+    g.invites.fetch().then(guildInvites => {
+      invites[g.id] = guildInvites;
+    });
+  });
 });
 
 client.on("guildMemberAdd", async (member) => {
   // To compare, we need to load the current invite list.
   const newInvites = await member.guild.invites.fetch();
   // This is the *existing* invites for the guild.
-  const cachedInvites = guildInvites.get(member.guild.id);
+  const ei = invites[member.guild.id];
 
+  invites[member.guild.id] = newInvites;
+  const guildInvites = await member.guild.invites.fetch();
   // Look through the invites, find the one for which the uses went up.
-  const usedInvite = newInvites.find(i => cachedInvites.get(i.code) < i.uses);
-  newInvites.each(inv => cachedInvites.set(inv.code, inv.uses));
-  guildInvites.set(member.guild.id, cachedInvites);
-  if (usedInvite !== null) {
-    addRole(member, usedInvite);
+  const invite = guildInvites.find(i => ei.get(i.code));
+  if (invite !== null) {
+    addRole(member, invite);
   }
 });
 
@@ -61,11 +53,14 @@ client.on('messageCreate', async (message) => {
   const command = args.shift().toLowerCase();
 
   switch (command) {
+    case "create":
+      await create(message, args);
+      break;
     case "add":
-      add(message, args);
+      await add(message, args);
       break;
     case "remove":
-      remove(message, args);
+      await remove(message, args);
       break;
     case "list":
       list(message);
@@ -80,14 +75,14 @@ client.on('messageCreate', async (message) => {
   }
 });
 
-function addRole(member, invite) {
+async function addRole(member, invite) {
   let rawdata = fs.readFileSync("invites.json");
   let _invites = JSON.parse(rawdata);
 
   try {
     const { roleID } = _invites[invite.code];
     if (roleID) {
-      var role = member.guild.roles.cache.find((role) => role.id === roleID);
+      const role = await member.guild.roles.fetch(roleID);
       member.roles.add(role);
     }
   } catch (err) {
@@ -100,7 +95,7 @@ function list(message) {
   message.reply({ content: '現在のリストです。', files: [attachment] });
 }
 
-function add(message, args) {
+async function add(message, args) {
   let rawdata = fs.readFileSync("invites.json");
   let _invites = JSON.parse(rawdata);
 
@@ -123,7 +118,7 @@ function add(message, args) {
     roleprefix.length,
     args[1].length - roleprefix.length - 1
   );
-  let role = message.guild.roles.cache.find((x) => x.id === roleID);
+  const role = await message.guild.roles.fetch(roleID);
 
   if (typeof role === undefined) {
     message.reply(`invalid role`);
@@ -139,4 +134,51 @@ function add(message, args) {
   );
 }
 
+/**
+ * 割当ロールを引数に、招待リンクの作成も一緒にやる
+ * @param {*} message
+ * @param {*} args
+ * @returns
+ */
+async function create(message, args) {
+  let rawdata = fs.readFileSync("invites.json");
+  let _invites = JSON.parse(rawdata);
+
+  if (args.length !== 1) {
+    message.reply(`not enough arguments`);
+    return;
+  }
+
+  // 招待リンクの作成
+  const guild = message.guild;
+  const channels = await guild.channels.fetch();
+  const welcome_channel = channels.find((c) => c.name === 'はじめに');
+
+  // 作成したリンクを使ってaddと同じことをやる
+  const roleprefix = "<@&";
+  if (args[0].substring(0, roleprefix.length) !== roleprefix) {
+    message.reply(`invalid role`);
+    return;
+  }
+  const roleID = args[0].substr(
+    roleprefix.length,
+    args[0].length - roleprefix.length - 1
+  );
+  const role = await guild.roles.fetch(roleID);
+
+  if (typeof role === undefined) {
+    message.reply(`invalid role`);
+    return;
+  }
+
+  const invite = await welcome_channel.createInvite({ maxAge: 0, unique: true, reason: role.name });
+  const inviteCode = invite.code;
+  _invites[inviteCode] = { roleID, name: role.name };
+
+  let data = JSON.stringify(_invites, null, 2);
+  fs.writeFileSync("invites.json", data);
+  message.reply(
+    `role @${role.name} を付与する招待リンクを作成しました。\n${invite.url}`
+  );
+}
 client.login(config.BOT_TOKEN);
